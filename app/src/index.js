a312dd36929695f5c9ff5e1f1ee60539b0a07497
vincedel <vincentdelevoye@gmail.com>
2019-05-14 15:10:29 +0200

    Add Request API + SessionStorage

--- app/src/index.js
+++ app/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import "./assets/scss/material-kit-react.scss";
 
 import ProtectedRoute from './security/ProtectedRoute'
+import SessionStore from './store/SessionStore';
 
 // pages for this product
 import Components from "./views/Components/Components.jsx";
@@ -13,16 +16,19 @@ import LandingPage from "./views/LandingPage/LandingPage.jsx";
 import ProfilePage from "./views/ProfilePage/ProfilePage.jsx";
 import LoginPage from "./views/LoginPage/LoginPage.jsx";
 
-var hist = createBrowserHistory();
+const hist = createBrowserHistory();
+const sessionStore = createStore(SessionStore);
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/landing-page" component={LandingPage} />
-      <ProtectedRoute path="/profile-page" component={ProfilePage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/" component={Components} />
-    </Switch>
-  </Router>,
+    <Provider store={sessionStore}>
+        <Router history={hist}>
+            <Switch>
+                <Route path="/landing-page" component={LandingPage} />
+                <ProtectedRoute path="/profile-page" component={ProfilePage} />
+                <Route path="/login" component={LoginPage} />
+                <Route path="/" component={Components} />
+            </Switch>
+        </Router>
+    </Provider>,
   document.getElementById("root")
 );
--- app/src/security/ProtectedRoute.jsx
+++ app/src/security/ProtectedRoute.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { Redirect } from 'react-router-dom'
 export default class ProtectedRoute extends React.Component{
     isAuthenticated() {
+        if (sessionStorage.getItem('user') != null) {
+            let user = JSON.parse(sessionStorage.getItem('user'));
+            console.log(user);
+            if (typeof user.token !== 'undefined') {
+                return true;
+            }
+        }
         return false;
     }
 
